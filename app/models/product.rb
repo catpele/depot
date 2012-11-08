28f28221037615d28ad35cf900dab513fd155da0
Chris Davis <cdavis.home@gmail.com>
2012-11-08 15:50:59 +0100

    Catalog

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,14 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+  
+  #Orders the product list alphabetically by their title
+  default_scope :order => 'title'
+  
+  #Validation
   validates :title, :description, :image_url, :presence =>true
-  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :title, :uniqueness => true
+  validates :title, :length => {:minimum => 10}
+  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :image_url, :format => {:with => %r{\.(gif|jpg|png)$}i,
     :message => 'Must be a URL for a GIF, JPG or PNG image'}
 end
